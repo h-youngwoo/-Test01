dcde023d8c9ec28e626965fa2a4bc150f733a90c
hyw <hyw@DESKTOP-SGQHUQ1>
2022-08-10 16:27:15 +0900

    최초 버전
    수업 테스트 버전

---
+++ chap01/src/chap01/Hello.java
@@ -0,0 +1,27 @@
+package chap01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// 글꼴 설정 방법: Window -> preference-> General -> Editors -> Text Editors -> Colors and Fonts
+		// 한 줄 주석
+		/* 여러줄 주석 */
+		// 주석 단축키: 블록설정 후  ctrl + / or ctrl + 7
+		
+		System.out.println("안녕");
+		
+		//ctrl + spacebar: 자동완성 단축키
+		
+		//sysout + ctrl + spacebar: System.out.println이 자동으로 완성
+		
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		System.out.println("4");
+		
+		//실행하려면 그 전에 변경내용이 저장되어야 실행됨.
+		//프로그램 화면 초기로 돌리는 방법: Window -> perspective -> Reset perspective
+		
+	}
+
+}
---
+++ chap01/src/chap01/Hi.java
@@ -0,0 +1,9 @@
+package chap01;
+
+public class Hi {
+	public static void main(String[] args) {
+		System.out.println("챕터 1을 끝맞쳤습니다.");
+		System.out.println("통합개발환경  영어로는 IDE");
+		System.out.println("우리는 이클립스를 사용합니다.");
+	}
+}

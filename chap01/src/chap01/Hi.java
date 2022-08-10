03a85266804e7e8b355ae4559bb24bb81246bf8e
hyw <hyw@DESKTOP-SGQHUQ1>
2022-08-10 16:36:10 +0900

    두 번째 버전
    새로운 테스트 문구 추가.

--- chap01/src/chap01/Hi.java
+++ chap01/src/chap01/Hi.java
@@ -5,5 +5,7 @@ public class Hi {
 		System.out.println("챕터 1을 끝맞쳤습니다.");
 		System.out.println("통합개발환경  영어로는 IDE");
 		System.out.println("우리는 이클립스를 사용합니다.");
+		
+		System.out.println("깃허브 테스트 중입니다.");
 	}
 }
